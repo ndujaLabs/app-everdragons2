e38ca567f2340753d8e8764f007ecc0e6b40dbc0
Francesco Sullo <sullof@gmail.com>
2022-02-20 10:38:39 -0800

    Minore fix

--- client/config/index.js
+++ client/config/index.js
@@ -1,6 +1,6 @@
 let isDev;
 if (typeof window !== "undefined") {
-  isDev = /localhost/.test(window.location.host);
+  isDev = /localhost/.test(window.location.host) || /stage/.test(window.location.pathname)
 } else if (typeof process !== undefined && process.env) {
   isDev = true;
 }
